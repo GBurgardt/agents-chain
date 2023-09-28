4507212269f8544f633c264d92135d69fa012a77
German Burgardt <german@getgloby.com>
2023-09-28 01:57:23 -0300

    added example

--- main.js
+++ main.js
@@ -1,80 +1,40 @@
 import { AgentChain } from "./models/agent-chain.js";
 
-// example:
+// Example:
 const agentChain = new AgentChain();
 
-agentChain
-  .addAgent(
-    "Eres un AI con una especialización profunda en el refactoring de código JavaScript con un enfoque en la mejora del diseño de sistemas. Debes identificar y rectificar problemas asociados con el diseño de sistemas en el código, aplicando patrones de diseño efectivos y buenas prácticas que aseguren un funcionamiento óptimo y eficiente del sistema. Además, tus soluciones deben ser simples pero efectivas, pudiendo implementar patrones de diseño cuando sean necesarios para una solución más efectiva.",
-    `Recibe este fragmento de código JavaScript. Refactoriza profundamente con un enfoque en solucionar problemas de diseño de sistemas. Identifica y rectifica problemas de diseño y aplica patrones de diseño efectivos y buenas prácticas que aseguren un funcionamiento óptimo y eficiente del sistema. Proporciona directamente el código JavaScript refactorizado sin explicaciones ni comentarios adicionales.`,
-    {
-      input: `function fetchData() {
-                      return fetch('https://api.example.com/data')
-                        .then(response => response.json())
-                        .then(data => console.log(data))
-                        .catch(error => console.error('Error fetching data: ', error));
-                    }
-                    
-                    fetchData();`,
-      output: `class DataFetcher {
-                      async fetchData() {
-                          try {
-                              const response = await fetch('https://api.example.com/data');
-                              const data = await response.json();
-                              console.log(data);
-                          } catch (error) {
-                              console.error('Error fetching data: ', error);
-                          }
-                      }
-                  }
-                  
-                  const fetcher = new DataFetcher();
-                  fetcher.fetchData();`,
-    }
-  )
-  .addAgent(
-    "Eres un AI especializado en simplificar y clarificar código JavaScript. Tu tarea principal es revisar el código refacturizado y optimizado para asegurarte de que es lo más simple y claro posible. Debes eliminar cualquier complejidad innecesaria, asegurarte de que las variables y funciones tienen nombres descriptivos y claros, y de que el código es fácil de leer y mantener.",
-    `Se te presenta un fragmento de código JavaScript que ha sido previamente refactorizado y optimizado. Tu tarea es revisarlo para asegurar su máxima simplicidad y claridad sin perder eficiencia ni funcionalidad. Elimina cualquier complejidad innecesaria y asegura que las variables y funciones tengan nombres descriptivos y claros. Proporciona solo el código resultante, sin comentarios explicativos adicionales.`,
-    {
-      input: `function addNumbers(a, b) {
-                      return a + b;
-                    }
-                    
-                    const result = addNumbers(3, 4);
-                    console.log(result);`,
-      output: `function add(a, b) {
-                      return a + b;
-                    }
-                    
-                    const sum = add(3, 4);
-                    console.log(sum);`,
-    }
-  );
+// Agent 1: Music Mood Detector
+agentChain.addAgent(
+  "You're an AI that determines the mood of text, like song lyrics. Analyze the text and provide the mood it conveys, such as happy, sad, energetic, etc.",
+  "Given a piece of text, identify and return the mood or emotion it conveys without additional explanation or comments.",
+  {
+    input: `Sunshine, lollipops and rainbows,
+            Everything that's wonderful is what I feel when we're together,
+            Brighter than a lucky penny,
+            When you're near the rain cloud disappears, dear,
+            And I feel so fine just to know that you are mine.`,
+    output: `Happy`,
+  }
+);
 
-agentChain
-  .execute(
-    `const agentChain = new AgentChain();
+// Agent 2: Playlist Creator
+agentChain.addAgent(
+  "Given a mood, you're an AI that generates a playlist with songs fitting that mood. Provide a list of songs that match the mood received as input.",
+  "Receive a mood descriptor, like 'happy', and return a list of songs that embody this mood without further explanation.",
+  {
+    input: `Happy`,
+    output: `1. "Happy" by Pharrell Williams
+            2. "Uptown Funk" by Mark Ronson ft. Bruno Mars
+            3. "I Gotta Feeling" by Black Eyed Peas
+            4. "Walking on Sunshine" by Katrina and The Waves
+            5. "Shake It Off" by Taylor Swift`,
+  }
+);
 
-    agentChain
-      .addAgent(
-        "Eres un AI especializado en simplificar y clarificar código JavaScript. Tu tarea principal es revisar el código refacturizado y optimizado para asegurarte de que es lo más simple y claro posible. Debes eliminar cualquier complejidad innecesaria, asegurarte de que las variables y funciones tienen nombres descriptivos y claros, y de que el código es fácil de leer y mantener.",
-        ,\`Se te presenta un fragmento de código JavaScript que ha sido previamente refactorizado y optimizado. Tu tarea es revisarlo para asegurar su máxima simplicidad y claridad sin perder eficiencia ni funcionalidad. Elimina cualquier complejidad innecesaria y asegura que las variables y funciones tengan nombres descriptivos y claros. Proporciona solo el código resultante, sin comentarios explicativos adicionales.\`,
-        {
-          input: \`function addNumbers(a, b) {
-                          return a + b;
-                        }
-                        
-                        const result = addNumbers(3, 4);
-                        console.log(result);\`,
-          output: \`function add(a, b) {
-                          return a + b;
-                        }
-                        
-                        const sum = add(3, 4);
-                        console.log(sum);\`,
-        }
-      );`
-  )
-  .then(result => {
-    // console.log(result);
-  });
+agentChain.execute(
+  `My mother was of the sky
+  My father was of the Earth
+  But I am of the universe
+  And you know what it's worth
+  `
+);
